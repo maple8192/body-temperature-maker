170b663bf25469862367c5b43afa015b05e8e36c
Maple8192 <57043077+maple8192@users.noreply.github.com>
2022-03-13 10:12:51 +0900

    シード値再生成、入力値保存機能を追加

--- script.js
+++ script.js
@@ -5,10 +5,25 @@ const LOOP_MAX = 5000;
 
 document.addEventListener("DOMContentLoaded", () => {
     // デフォルト値を設定
+    let savedAverage = localStorage.getItem("average");
+    document.getElementById("average").value = savedAverage == null ? "36.5" : savedAverage;
+    let savedMin = localStorage.getItem("min");
+    document.getElementById("min").value = savedMin == null ? "36.0" : savedMin;
+    let savedMax = localStorage.getItem("max");
+    document.getElementById("max").value = savedMax == null ? "36.9" : savedMax;
+    let savedDeviation = localStorage.getItem("deviation");
+    document.getElementById("deviation").value = savedDeviation == null ? "0.3" : savedDeviation;
+    let savedSeed = localStorage.getItem("seed");
+    document.getElementById("seed").value = savedSeed == null ? Math.floor(Math.random() * (UINT_MAX - 1) + 1) : savedSeed;
+
     let date = new Date();
     document.getElementById("month").value = date.getMonth() + 1;
     document.getElementById("date").value = date.getDate();
-    document.getElementById("seed").value = Math.floor(Math.random() * (UINT_MAX - 1) + 1);
+
+    // シード値再生成ボタンが押されたときの処理
+    document.getElementById("seed-reset").addEventListener("click", () => {
+        document.getElementById("seed").value = Math.floor(Math.random() * (UINT_MAX - 1) + 1);
+    });
 
     // 生成ボタンが押されたときの処理
     document.getElementById("make").addEventListener("click", () => {
@@ -25,6 +40,13 @@ document.addEventListener("DOMContentLoaded", () => {
             alert("無効な入力があります。");
             return;
         }
+
+        // 設定値を保存
+        localStorage.setItem("average", document.getElementById("average").value);
+        localStorage.setItem("min", document.getElementById("min").value);
+        localStorage.setItem("max", document.getElementById("max").value);
+        localStorage.setItem("seed", document.getElementById("seed").value);
+        localStorage.setItem("deviation", document.getElementById("deviation").value);
 
         // 体温生成
         let random = new Random(parseInt(document.getElementById("seed").value) % (UINT_MAX + 1));
